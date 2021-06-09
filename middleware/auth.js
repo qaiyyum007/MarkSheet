8d77ae58ea670ad36342cd7436f6f7ff25cd43e2
abdul <aqaiyyum007@gmail.com>
2021-06-09 11:28:26 +0530

    make final change

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,10 @@
 import jwt from "jsonwebtoken"
 
   export const isAuth = (req, res, next) => {
-    // const token = req.headers.authorization;
 
     const token=req.headers.token
   
     if (token) {
-      // const onlyToken = token.slice(7, token.length);
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, candidate) => {
         if (err) {
           return res.status(403).send({ message: 'Invalid Token' });
